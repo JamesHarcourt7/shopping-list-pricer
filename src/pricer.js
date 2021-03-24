ff2da43118a90ce5fe582c5796fac0fa5a861659
JamesHarcourt7 <jh3g20@soton.ac.uk>
2021-03-24 22:14:02 +0000

    Made the server better? I think? It handles parameters now- next need to actually remember what this project does and make selenium do something other than  vegetate.

--- src/pricer.js
+++ src/pricer.js
@@ -1,14 +1,51 @@
 // Shamelessly stolen simple server code from the Internet.
 
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const webdriver = require("selenium-webdriver");
+const { Driver } = require("selenium-webdriver/chrome");
+const request = require("request");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('i made this because im poor and lazy');
+    var request;
+    if (req.url == "/") {
+        request = "index.html";
+    } else {
+        request = req.url.substring(1);
+    }
+    
+    if (request == "index.html") {
+        res.statusCode = 200;
+        var request = req.url.substring(1);
+
+        console.log("request: " + req.url);
+        res.setHeader("Content-Type", "text/html");
+
+        fs.createReadStream("src/index.html").pipe(res);
+
+    } else if (request.startsWith("?param=")) {
+        res.end(request.substring(7));
+    }
+    else {
+        res.end("Invalid URL mate.");
+    }
 });
 
 server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}/`);
 });
+
+(async function main() {
+    let url = "https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]=baguette";
+    let driver = new webdriver.Builder()
+            .forBrowser("firefox")
+            .build();
+    try {
+        await driver.get("http://www.google.com/ncr");
+        await driver.findElement(webdriver.By.name("q")).sendKeys("webdriver");
+        await driver.wait(webdriver.until.titleIs("webdriver - Google Search"), 1000);
+    } finally {
+        await driver.quit();
+    }
+})
